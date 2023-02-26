e4edde72e7a00d7fcda68b40d015780441b33d1e
BulingBulingWei <257459102@qq.com>
2023-02-26 20:51:30 +0800

    修改了响应式布局的bug

--- src/pages/SearchDetail/index.js
+++ src/pages/SearchDetail/index.js
@@ -193,45 +193,6 @@ export default function SearchDetail() {
       author: ["Shi Yang"],
       time: "2003 Jan",
     },
-    {
-      id: 11,
-      doi: [
-        "S0168-9525(02)00011-2 [pii]",
-        "10.1016/s0168-9525(02)00011-2 [doi]",
-      ],
-      pmcid: "12493243",
-      title: "Macro effects of microRNAs in plants.",
-      abstract:
-        "MicroRNAs (miRNAs) are 20- to 22-nucleotide fragments that regulate expression of mRNAs that have complementary sequences. They are numerous and widespread among eukaryotes being conserved throughout evolution. The few miRNAs that have been fully characterized were found in Caenorhabditis elegans and are required for development. Recently a study of miRNAs isolated from Arabidopsis showed that here also developmental genes are putative regulatory targets. A role for miRNAs have in plant development is supported by the developmental phenotypes of mutations in the genes required for miRNA processing.",
-      author: ["Kidner Catherine A", "Martienssen Robert A"],
-      time: "2003 Jan",
-    },
-    {
-      id: 12,
-      doi: [
-        "S0168-9525(02)00005-7 [pii]",
-        "10.1016/s0168-9525(02)00005-7 [doi]",
-      ],
-      pmcid: "12493242",
-      title: "Mammalian RNAi for the masses.",
-      abstract:
-        "Just a couple of years ago only biologists working with plants or Caenorhabditis elegans could use RNA-mediated interference (RNAi) technology to gain insight into gene function. However the recent groundbreaking discovery that in vitro synthesized 21- to 23-nucleotide double-stranded RNAs can act as small interfering RNAs (siRNAs) to elicit gene-specific inhibition in mammalian cells has made RNAi possible in mammalian systems too. Reported only a year ago mammalian RNAi is already changing our way of studying gene function in higher eukaryotes. And a recent exciting advance allows delivery of siRNAs into mammalian cells by a DNA vector. In addition to providing a low-cost alternative to the chemically synthesized siRNAs this DNA-vector-based strategy is capable of mediating stable target gene inhibition thus allowing gene function analysis over an extended period of time.",
-      author: ["Shi Yang"],
-      time: "2003 Jan",
-    },
-    {
-      id: 13,
-      doi: [
-        "S0168-9525(02)00011-2 [pii]",
-        "10.1016/s0168-9525(02)00011-2 [doi]",
-      ],
-      pmcid: "12493243",
-      title: "Macro effects of microRNAs in plants.",
-      abstract:
-        "MicroRNAs (miRNAs) are 20- to 22-nucleotide fragments that regulate expression of mRNAs that have complementary sequences. They are numerous and widespread among eukaryotes being conserved throughout evolution. The few miRNAs that have been fully characterized were found in Caenorhabditis elegans and are required for development. Recently a study of miRNAs isolated from Arabidopsis showed that here also developmental genes are putative regulatory targets. A role for miRNAs have in plant development is supported by the developmental phenotypes of mutations in the genes required for miRNA processing.",
-      author: ["Kidner Catherine A", "Martienssen Robert A"],
-      time: "2003 Jan",
-    },
   ]);
   const [graphData, setGraphData] = useState({
     nodes: [
@@ -2159,6 +2120,32 @@ export default function SearchDetail() {
     myChart.setOption(graphOption);
   }
 
+  //点击搜索
+  const handleSearch = () => {
+    const fetchData = async () => {
+      const options = {
+        url: "",
+        method: "GET",
+        headers: {
+          "content-type": "",
+        },
+        data: {},
+      };
+      const res = await axios(options);
+
+      if (res.data.code === 200) {
+        navigate(`SearchDetail`);
+      } else {
+        toastController({
+          mes: res.data.message,
+          timeout: 1000,
+        });
+      }
+    };
+    navigate(`SearchDetail`);
+    // fetchData();
+  };
+
   const enterKeyUp = (e) => {
     if (e.keyCode === 13) {
       // handleSearch();
@@ -2166,22 +2153,92 @@ export default function SearchDetail() {
   };
 
   return (
-    <div className="h-fit w-full bg-blue-5 relative md:h-full ">
-      {/* 搜索框+关系图+图例 */}
+    <div
+      className={`h-fit w-full bg-blue-5 relative md:h-full ${
+        showGraph === false ? "md:flex md:justify-center" : ""
+      }`}
+    >
+      {/* (关系图)搜索框+关系图+图例 */}
       <div
         id="graph"
         className={`h-96 w-full flex justify-center items-center bg-blue-50 shadow-inner
         md:h-full ${showGraph === true ? "" : "hidden"}`}
       ></div>
 
+      {/* 论文详情 */}
+      {paperList !== undefined &&
+        paperList !== null &&
+        paperSelectedId !== undefined &&
+        paperList.map((item) => {
+          if (item.id === paperSelectedId) {
+            return (
+              // 论文详情
+              <div
+                className={`h-fit w-full 
+                transition-all duration-1000 
+                ${
+                  showGraph === false
+                    ? // 无图
+                      "md:h-full md:order-2 md:w-7/12 "
+                    : showRight === true
+                    ? // 有图且显示论文详情
+                      "md:w-60 lg:w-80 xl:w-85 2xl:w-100 md:absolute md:top-0 md:right-0 md:h-full md:min-h-0"
+                    : // 不显示右边的论文详情
+                      "md:w-0 md:absolute md:top-0 md:right-0 md:h-full md:min-h-0"
+                }
+                `}
+              >
+                {/* 点击向右回缩按钮 */}
+                <div
+                  className={`h-0 w-5 bg-gray-100 flex justify-center items-center shadow-lg
+                md:h-12 md:absolute md:top-40 md:-left-5 md:rounded-l ${
+                  showGraph === true ? "" : "hidden"
+                }`}
+                  onClick={() => {
+                    setShowRight(!showRight);
+                  }}
+                >
+                  <Svg1
+                    className={`hidden transform duration-700 md:block ${
+                      showRight === true ? "" : "rotate-180"
+                    }`}
+                  ></Svg1>
+                </div>
+                {/* 论文内容滚动面板 container */}
+                <div
+                  className="h-full w-full bg-gray-50 pl-3 py-3 shadow-lg 
+                    md:overflow-y-scroll"
+                >
+                  <h1 className="text-xl font-bold block mb-2">{item.title}</h1>
+                  {item.author !== undefined &&
+                    item.author !== null &&
+                    item.author.map((aut) => {
+                      return <p className="text-gray-600 inline pr-2">{aut}</p>;
+                    })}
+                  <p className="pt-1 text-gray-600">{item.time}</p>
+                  <div className="h-4 w-full"></div>
+                  <div className="inline-block h-6 w-fit mr-3">Open in:</div>
+                  <div className="h-6 w-6 rounded-full text-white bg-gray-500 inline-block">
+                    <div className=" h-fit w-fit mx-auto">doi</div>
+                  </div>
+                  <div className="h-4 w-full"></div>
+                  <p className="text-sky-800 font-bold">Abstract:</p>
+                  <p>{item.abstract}</p>
+                </div>
+              </div>
+            );
+          }
+          return <></>;
+        })}
+
       {/* 相关论文选项列表 */}
       <div
-        className={`h-80 w-full select-none 
+        className={`h-fit w-full select-none 
         transition-all duration-1000 shadow-2xl  
         ${
           showGraph === false
             ? // 无图
-              "md:h-full md:w-60 lg:w-72 xl:w-80 2xl:w-96 md:relative md:top-0 md:-right-72 "
+              " md:order-1 md:mr-1 md:h-full md:w-3/12"
             : // 有图
             showLeft === true
             ? "md:w-60 lg:w-72 xl:w-80 2xl:w-96 md:absolute md:top-0 md:left-0 md:h-full md:min-h-0"
@@ -2325,69 +2382,8 @@ export default function SearchDetail() {
             })}
         </div>
       </div>
-
-      {/* 论文详情 */}
-      {paperList !== undefined &&
-        paperList !== null &&
-        paperSelectedId !== undefined &&
-        paperList.map((item) => {
-          if (item.id === paperSelectedId) {
-            return (
-              <div
-                className={`h-fit w-full 
-                transition-all duration-1000 
-                ${
-                  showGraph === false
-                    ? // 无图
-                      "md:h-full md:w-5/12  md:absolute md:top-0 md:right-52"
-                    : showRight === true
-                    ? "md:w-60 lg:w-80 xl:w-85 2xl:w-100 md:absolute md:top-0 md:right-0 md:h-full md:min-h-0"
-                    : "md:w-0 md:absolute md:top-0 md:right-0 md:h-full md:min-h-0"
-                }
-                `}
-              >
-                {/* 点击向右回缩按钮 */}
-                <div
-                  className={`h-0 w-5 bg-gray-100 flex justify-center items-center shadow-lg
-                md:h-12 md:absolute md:top-40 md:-left-5 md:rounded-l ${
-                  showGraph === true ? "" : "hidden"
-                }`}
-                  onClick={() => {
-                    setShowRight(!showRight);
-                  }}
-                >
-                  <Svg1
-                    className={`hidden transform duration-700 md:block ${
-                      showRight === true ? "" : "rotate-180"
-                    }`}
-                  ></Svg1>
-                </div>
-                {/* 论文内容滚动面板 container */}
-                <div
-                  className="h-full w-full bg-gray-50 pl-3 py-3 shadow-lg 
-                    md:overflow-y-scroll"
-                >
-                  <h1 className="text-xl font-bold block mb-2">{item.title}</h1>
-                  {item.author !== undefined &&
-                    item.author !== null &&
-                    item.author.map((aut) => {
-                      return <p className="text-gray-600 inline pr-2">{aut}</p>;
-                    })}
-                  <p className="pt-1 text-gray-600">{item.time}</p>
-                  <div className="h-4 w-full"></div>
-                  <div className="inline-block h-6 w-fit mr-3">Open in:</div>
-                  <div className="h-6 w-6 rounded-full text-white bg-gray-500 inline-block">
-                    <div className=" h-fit w-fit mx-auto">doi</div>
-                  </div>
-                  <div className="h-4 w-full"></div>
-                  <p className="text-sky-800 font-bold">Abstract:</p>
-                  <p>{item.abstract}</p>
-                </div>
-              </div>
-            );
-          }
-          return <></>;
-        })}
     </div>
   );
 }
+
+// md:w-72 lg:w-80 xl:w-85 2xl:w-96
