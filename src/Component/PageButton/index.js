5ef559869ea5fd081a3950aface9ab9aeb9081bd
BulingBulingWei <257459102@qq.com>
2023-03-12 15:16:04 +0800

    修改了分页按钮在移动端设备上的显示问题，修改了移动端设备的布局设计，使显示论文摘要的box的高度可通过拖拽去调整

--- src/Component/PageButton/index.js
+++ src/Component/PageButton/index.js
@@ -7,7 +7,10 @@ import Toast from "../toast";
 export default function PageButton(props) {
   return (
     <>
-      <div className="h-7 w-fit relative px-3 mx-2 rounded bg-gray-50 shadow-md">
+      <div
+        className="h-7 w-fit relative rounded bg-gray-50 shadow-md
+      px-1 mx-1 xl:px-2 2xl:px-3 2xl:mx-2 "
+      >
         <p className="leading-7 text-center text-gray-600">
           {props.content.toString()}
         </p>
